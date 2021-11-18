f4e4dc1db1a8520a33bfd0cb8864c256f1007df4
dhanraj <dhanraj@aequalisys.com>
2021-11-18 11:35:32 +0530

    search functionality implemented

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import Card from "../../components/Card";
 import { UserContext } from "../../userContext";
+import "./index.css";
 
 const Home = () => {
   const history = useHistory();
   const { userData } = useContext(UserContext);
+  const [userName, setUserName] = useState("");
+  const [filteredUserData, setFilteredUserData] = useState();
+  useEffect(() => {
+    console.log("running");
+    const filteredData = userData.filter((data) =>
+      data.name.includes(userName)
+    );
+    setFilteredUserData(filteredData);
+  }, [userName, userData]);
 
   return (
-    <div className="CardsContainer">
-      {userData?.map((user) => {
-        return (
-          <div onClick={() => history.push(`/${user.id}`)}>
-            <Card
-              key={user.id}
-              id={user.id}
-              name={user.name}
-              img={user.Image}
-            />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="searchContainer">
+        <input
+          type="text"
+          placeholder="Search User by name"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+      </div>
+      <div className="CardsContainer">
+        {filteredUserData?.map((user) => {
+          return (
+            <div key={user.id} onClick={() => history.push(`/${user.id}`)}>
+              <Card id={user.id} name={user.name} img={user.Image} />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
