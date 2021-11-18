c972ab3c19e9796eddb6a4dce8370586ef0074ea
dhanraj <dhanraj@aequalisys.com>
2021-11-18 10:57:13 +0530

    rejected and shortlisted unctionalities implemented

--- src/pages/Singleuser/index.jsx
+++ src/pages/Singleuser/index.jsx
@@ -1,17 +1,30 @@
 import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router";
 import { UserContext } from "../../userContext";
 import "./index.css";
 
 const SingleUser = (props) => {
   const { match } = props;
-  const { userData } = useContext(UserContext);
+  const history = useHistory();
+  const { userData, shortlisted, setShortlisted, rejected, setRejected } =
+    useContext(UserContext);
   const [user, setUser] = useState([]);
 
   useEffect(() => {
+    setUser(userData.filter((data) => data.id === match.params.id));
     if (userData.length > 0) {
-      setUser(userData.filter((data) => data.id === match.params.id));
     }
   }, []);
+
+  const handleShortlist = async (data) => {
+    await setShortlisted([...shortlisted, data[0]]);
+    history.push("/");
+  };
+
+  const handleReject = async (data) => {
+    await setRejected([...rejected, data[0]]);
+    await history.push("/");
+  };
 
   return (
     <div className="singleUserContainer">
@@ -27,8 +40,18 @@ const SingleUser = (props) => {
           <li>Education</li>
 
           <div className="btnContainer">
-            <button className="shortListBtn btn">Shortlist</button>
-            <button className="rejectBtn btn">Reject</button>
+            <button
+              className="shortListBtn btn"
+              onClick={() => handleShortlist(user)}
+            >
+              Shortlist
+            </button>
+            <button
+              className="rejectBtn btn"
+              onClick={() => handleReject(user)}
+            >
+              Reject
+            </button>
           </div>
         </ul>
       </div>
